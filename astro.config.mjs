a724b49d3acf3275b30f67d82a1d1d3dca0216e4
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-02 02:13:20 -0300

    fix: rm vercel image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ export default defineConfig({
   output: "hybrid",
   adapter: vercel({
     imageService: true,
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    },
     isr: {
       // caches all pages on first request and saves for 1 week
       expiration: 60 * 60 * 24 * 6,
