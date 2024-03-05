c6f1d25339c3010268233fb5e85f4b00a2bb5e79
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-05 20:03:41 -0300

    feat: new collection utils and types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,11 @@
+import type { CollectionEntry, CollectionKey } from "astro:content";
+
 export type Navlinks = {
   href: string;
   label: string;
 };
+
+export type Project = CollectionEntry<"projects">;
+export type Blog = CollectionEntry<"blog">;
+export type Carreer = CollectionEntry<"carreer">;
+export type Collection = CollectionKey;
