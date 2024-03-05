4f73adf3a109c39f56be067462b9f6361f74723e
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-04 23:09:14 -0300

    feat: new contact form
    Using react-hook-form, Zod validation, api endpoint, Resend service with React email template. The component js is loaded when it's visible

---
+++ src/pages/api/sendEmail.json.ts
@@ -0,0 +1,57 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+import { emailSchema } from "@/lib/zod-schema";
+import { EmailTemplate } from "@/components/EmailTemplate";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const email = import.meta.env.RESEND_EMAIL;
+
+export const POST: APIRoute = async ({ params, request }) => {
+  const data = await request.json();
+  const parsedData = emailSchema.safeParse(data);
+
+  if (!parsedData.success) {
+    return new Response(
+      JSON.stringify({
+        message: "Incorrect data.",
+      }),
+      {
+        status: 400,
+        statusText: "Incorrect data.",
+      }
+    );
+  }
+
+  const send = await resend.emails.send({
+    from: email,
+    to: email,
+    subject: parsedData.data.subject,
+    react: EmailTemplate({
+      email: parsedData.data.email,
+      message: parsedData.data.message,
+      subject: parsedData.data.subject,
+    }) as React.ReactElement,
+  });
+
+  if (send.data) {
+    return new Response(
+      JSON.stringify({
+        message: send.data,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      }
+    );
+  }
+
+  return new Response(
+    JSON.stringify({
+      message: send.error,
+    }),
+    {
+      status: 500,
+      statusText: "Internal Server Error",
+    }
+  );
+};
