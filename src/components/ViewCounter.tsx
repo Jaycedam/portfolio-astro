8be9c2250c58f6fe62d8c6ca70f432ae5b5202de
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-13 21:49:14 -0300

    chore: move pathname getter to component

--- src/components/ViewCounter.tsx
+++ src/components/ViewCounter.tsx
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 
-export default function ViewCounter({ pathname }: { pathname: string }) {
+export default function ViewCounter() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    const pathname = window.location.pathname;
     const api = "/api/pagecount.json";
 
     fetch(api, { method: "POST", body: JSON.stringify({ slug: pathname }) })
       .then((res) => res.json())
       .then((res) => setCount(res.message));
   }, []);
-  return (
-    <p>
-      {count} views for {pathname}
-    </p>
-  );
+  return <p>{count} views</p>;
 }
