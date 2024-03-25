7ddb14044474e48977022ccf62145366769d3ed2
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-25 15:24:58 -0300

    feat: svelte view counter

---
+++ src/components/ViewCounter.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let views: number = 0;
+
+  onMount(async () => {
+    const pathname = window.location.pathname;
+    const api = "/api/pagecount.json?slug=" + pathname;
+
+    fetch(api)
+      .then((res) => res.json())
+      .then((res) => (views = res.message));
+  });
+</script>
+
+<p>{views} views</p>
