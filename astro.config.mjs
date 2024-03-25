09d4603235bb5bb1eee3bc25d600f8a2f52fcff4
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-03-25 15:24:42 -0300

    feat(deps): added svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,32 +6,28 @@ import vercel from "@astrojs/vercel/serverless";
 import expressiveCode from "astro-expressive-code";
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jordancortes.dev",
   output: "hybrid",
   adapter: vercel(),
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    expressiveCode({
-      themes: ["dracula"],
-      styleOverrides: {
-        codeBackground: "#18181b",
-        borderColor: "#27272a",
-        frames: {
-          editorTabBarBackground: "#18181b",
-          editorActiveTabBackground: "#27272a",
-          terminalBackground: "#18181b",
-          terminalTitlebarBackground: "#18181b",
-          editorActiveTabIndicatorTopColor: "#3f3f46",
-          terminalTitlebarBorderBottomColor: "#18181b",
-        },
-      },
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), expressiveCode({
+    themes: ["dracula"],
+    styleOverrides: {
+      codeBackground: "#18181b",
+      borderColor: "#27272a",
+      frames: {
+        editorTabBarBackground: "#18181b",
+        editorActiveTabBackground: "#27272a",
+        terminalBackground: "#18181b",
+        terminalTitlebarBackground: "#18181b",
+        editorActiveTabIndicatorTopColor: "#3f3f46",
+        terminalTitlebarBorderBottomColor: "#18181b"
+      }
+    }
+  }), mdx(), sitemap(), svelte()]
 });
