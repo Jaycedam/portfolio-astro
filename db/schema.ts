1556ea921c9e5ca3bd29a0e4cf5d39195ae66439
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2024-09-22 17:51:53 -0300

    chore: rm counter and db

--- db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
-
-export const page = sqliteTable("page", {
-  id: integer("id").primaryKey(),
-  slug: text("slug").unique().notNull(),
-  views: integer("views").default(0),
-});
