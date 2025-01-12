d2fa7714408210f596210bacb84dcb5493c190eb
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-01-12 04:09:45 -0300

    feat: blog img optional

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,15 +20,18 @@ export const projectsCollection = defineCollection({
 export const blogCollection = defineCollection({
     loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/data/blog" }),
     // Type-check frontmatter using a schema
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        // Transform string to Date object
-        date: z.coerce.date(),
-        updatedDate: z.coerce.date().optional(),
-        tags: z.string().array(),
-    }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            // Transform string to Date object
+            date: z.coerce.date(),
+            updatedDate: z.coerce.date().optional(),
+            tags: z.string().array(),
+            image: image().optional(),
+        }),
 });
+
 export const collections = {
     projects: projectsCollection,
     blog: blogCollection,
